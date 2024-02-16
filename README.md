ef97de9a0c0b5c148af4a69a37630dd4a0800199
kristian <kristian.valto@gmail.com>
2024-02-16 03:06:44 +0200

    split ffmpeg to 3 parts for nice quality with transparent support and input validation

--- README.md
+++ README.md
@@ -1,21 +1,23 @@
 # Instructions
 
 - Install ffmpeg
-- Put the app in a folder with images
+- Put the app in a folder with images that are sized same
 - Launch it
 - Enjoy gif
 
 # Configure
-- Default values: .png files, 30fps, shows log
+- Default values: png files, 30fps, shows log
 - Edit by creating config.txt in same folder as the app
-- 1st line defines filetype with dot at start e.g ".png"
+- 1st line defines filetype without dot at start e.g "png"
 - 2nd line defines fps
 - 3rd line defines debug mode if any text but whitespace is set
 
 # How it works
 - ffmpeg is a wonderful tool but defining input range for gifs or videos can be a pain when files are not named in a specific pattern.
 - This app temporarily renames all files in folder matching filetype and runs ffmpeg on them. Then renames them back. Sets the ordering for gif with default cmp.Compare ignoring case sensitivity.
-
+- in order to deal with transparency we create a tile of input images and palette out of them from which the gif will be made of.
 # TODOs
 - maybe mp4 if waifu wants
-- sanitize input in case this gets on a server
+- size validation
+- maybesanitize input in case this gets on a server
+- jpegs and other formats to test and try?
--- gifMaker/gifMaker.go
+++ gifMaker/gifMaker.go
@@ -9,6 +9,7 @@ import (
 	"os/exec"
 	"slices"
 	"strings"
+	"regexp"
 )
 
 func main() {
@@ -18,24 +19,42 @@ func main() {
 	if len(nameMap) == 0 {
 		prln("no files with type '"+filetype+"' found. Exiting", dlog)
 		return
+		// todo validate sizes
+	}
+	tileName, errTile := genGlobalTile(filetype, dlog)
+	if errTile != nil {
+		prln(errTile.Error(), dlog)
+		nameBack(&nameMap)
+		os.Exit(1)
 	}
 
-	cmdString := "ffmpeg -framerate " + fps + " -i %6d" + filetype + " output.gif -y"
-	prln("cmd: "+cmdString, dlog)
-	parts := strings.Fields(cmdString)
-	cmd := exec.Command(parts[0], parts[1:]...)
-	outStdAndErr, err := cmd.CombinedOutput()
+	paletteName, errPalette := genPalette(filetype, tileName, dlog)
+	if errPalette == nil {
+		errGif := genGif(filetype, paletteName, fps, dlog)
+		if errGif != nil {
+			prln("output.gif generated", dlog)
+		} else {
+			prln(errGif.Error(), dlog)
+		}
+	}
 
-	if err != nil {
-		nameBack(&nameMap)
-		prln(string(outStdAndErr), dlog)
-		prln(err.Error(), dlog)
-		os.Exit(1)
-	} else {
-		// to view output if needed
-		// fmt.Printf("%s\n", outStdAndErr)
-		nameBack(&nameMap)
-		prln("output.gif generated", dlog)
+	// Clean up. reverse rename of nameMap and delete tmp and palette files
+	nameBack(&nameMap)
+	if errTile == nil {
+		e := os.Remove(tileName)
+		if  e != nil {
+			prln("failed to delete temp file for tile. File can be deleted manually " + tileName, dlog)
+		} else {
+			prln("tmp tile file deleted successfully " + tileName, dlog)
+		}
+	}
+	if errPalette == nil {
+		e := os.Remove(paletteName)
+		if e != nil {
+			prln("failed to delete palette file. File can be deleted manually " + paletteName, dlog)
+		} else {
+			prln("tmp palette file deleted successfully " + paletteName, dlog)	
+		}
 	}
 	if dlog {
 		prln("Press Enter to exit", dlog)
@@ -45,8 +64,69 @@ func main() {
 	}
 }
 
+func genGlobalTile(filetype string, dlog bool) (string, error) {
+	tileName := "tmp.png"
+	parts := []string{
+		"ffmpeg",
+		"-y",
+		"-hide_banner",
+		"-i",
+		"%6d." + filetype,
+		"-vf",
+		"format=rgb24,tile=10x10:color=black",
+		"-frames:v",
+		"1",
+		tileName,
+	}
+	prln("part 1. create tile: " + strings.Join(parts[:], " "), dlog)
+	cmd := exec.Command(parts[0], parts[1:]...)
+	outStdAndErr, err := cmd.CombinedOutput()
+	if (err != nil && dlog) {
+		fmt.Println(string(outStdAndErr))
+	}
+	return tileName, err
+}
+
+func genPalette(filetype string, tileName string, dlog bool) (string, error) {
+	palName := "palette.png"
+	parts := []string{
+		"ffmpeg",
+		"-y",
+		"-hide_banner",
+		"-i",
+		tileName,
+		"-vf",
+		"palettegen=max_colors=64:reserve_transparent=1:stats_mode=single",
+		palName,
+	}
+	prln("part 2. create palette: " + strings.Join(parts[:], " "), dlog)
+	cmd := exec.Command(parts[0], parts[1:]...)
+	err := cmd.Run()
+	return palName, err
+}
+
+func genGif(filetype string, paletteName string, fps string, dlog bool) error {
+	parts := []string{
+		"ffmpeg",
+		"-y",
+		"hide_banner",
+		"-framerate",
+		fps,
+		"-i",
+		"%6d." + filetype,
+		"-i",
+		paletteName,
+		"-filter_complex",
+		"split[s0][s1]; [s0]palettegen= max_colors=256: stats_mode=single: reserve_transparent=on: transparency_color=ffffff[p]; [s1][p]paletteuse=new=1",
+		"output.gif",
+	}
+	prln("part 3. create Gif: " + strings.Join(parts[:], " "), dlog)
+	cmd := exec.Command(parts[0], parts[1:]...)
+	return cmd.Run()
+}
+
 func setConfig() (string, string, bool) {
-	var filetype = ".png"
+	var filetype = "png"
 	var fps = "30"
 	var debug = true
 
@@ -59,12 +139,12 @@ func setConfig() (string, string, bool) {
 		for scanner.Scan() {
 			if line == 0 {
 				filetypeFound := strings.ToLower(strings.Trim(scanner.Text(), " "))
-				if len(filetypeFound) != 0 {
+				if len(filetypeFound) != 0 && regexp.MustCompile(`^[a-zA-Z0-9]*$`).MatchString(filetypeFound) {
 					filetype = filetypeFound
 				}
 			} else if line == 1 {
 				fpsFound := strings.Trim(scanner.Text(), " ")
-				if len(fpsFound) != 0 {
+				if len(fpsFound) != 0 && regexp.MustCompile(`^[0-9]*$`).MatchString(fpsFound) {
 					fps = fpsFound
 				}
 			} else if line == 2 {
@@ -78,9 +158,10 @@ func setConfig() (string, string, bool) {
 			line++
 		}
 	}
-	prln("filetype "+filetype, debug)
-	prln("fps "+fps, debug)
-	prln("debug "+fmt.Sprint(debug), debug)
+	prln("running with parameters:", debug)
+	prln("	filetype "+filetype, debug)
+	prln("	fps "+fps, debug)
+	prln("	debug "+fmt.Sprint(debug), debug)
 	return filetype, fps, debug
 }
 
@@ -101,18 +182,19 @@ func handleInputFiles(filetype string, dlog bool) map[string]string {
 		curName := e.Name()
 		if strings.HasSuffix(strings.ToLower(curName), filetype) {
 			i++
-			newName := fmt.Sprintf("%06d", i) + filetype
+			newName := fmt.Sprintf("%06d.", i) + filetype
 			nameMap[newName] = curName
 			os.Rename(curName, newName)
 		}
 	}
 
 	if dlog {
-		fmt.Println("filemap to be giffified below")
+		fmt.Print("Found files to convert into gif. Renaming them temporarily: ")
 		fmt.Println(&nameMap)
 	}
 	return nameMap
 }
+
 func nameBack(nameMap *map[string]string) {
 	for temp, og := range *nameMap {
 		os.Rename(temp, og)
